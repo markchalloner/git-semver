2cd9931ab90416daf982bd9f1e58cf5a1ae22915
Fernando Collova <fernandocollova@gmail.com>
2019-04-11 17:17:42 -0300

    Removed --force to avoid rewriting

--- plugins/90-major_and_minor_tag.sh
+++ plugins/90-major_and_minor_tag.sh
@@ -7,21 +7,24 @@ function run() {
     local minor_number=$(echo "$version_new" | cut --delimiter="." --fields=2)
     local major_number=$(echo "$version_new" | cut --delimiter="." --fields=1)
 
-    if git tag --list | grep -xq "$major_number"".""$minor_number"
+    minor_tag="$major_number"".""$minor_number"
+    if git tag --list | grep -xq "$minor_tag"
     then
-        echo "Recreating minor tag ""$major_number"".""$minor_number"
+        echo "Recreating minor tag $minor_tag"
+        git tag --delete "$minor_tag"
     else
         echo "Creating minor tag ""$major_number"".""$minor_number"
     fi
-    git tag --force "$major_number"".""$minor_number"
+    git tag "$minor_tag"
 
     if git tag --list | grep -xq "$major_number"
     then
-        echo "Recreating major tag ""$major_number"
+        echo "Recreating major tag $major_number"
+        git tag --delete "$major_number"
     else
         echo "Creating major tag ""$major_number"
     fi
-    git tag --force "$major_number"
+    git tag "$major_number"
 
     return 0
 }
